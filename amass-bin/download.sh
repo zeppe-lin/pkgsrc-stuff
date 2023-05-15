4bd566b38dbdfbde712a2aedce968bc6182c6bf7
Alexandr Savca <alexandr.savca89@gmail.com>
2023-05-15 18:36:54 +0300

    amass-bin: 3.22.2 -> 3.23.2

--- amass-bin/download.sh
+++ amass-bin/download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./Pkgfile
 wget -O $name-$version-x86_64.zip \
-https://github.com/OWASP/Amass/releases/download/v$version/amass_linux_amd64.zip
+	https://github.com/owasp-amass/amass/releases/download/v$version/amass_Linux_amd64.zip
