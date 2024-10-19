ac1390b3f0510dc81deeafb63745d0c0b0498dba
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-19 11:20:42 +0300

    avidemux276-appimage: apply AppRun.patch, improve launcher.sh and desktop file, relbump

--- avidemux276-appimage/launcher.sh
+++ avidemux276-appimage/launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 LD_PRELOAD="/usr/lib/apulse/libpulse-simple.so.0:/usr/lib/apulse/libpulse.so.0:/usr/lib/apulse/libpulse.so" \
-/opt/avidemux/avidemux-2.7.6.AppImage
+/opt/avidemux/avidemux-2.7.6.AppImage "$@"
