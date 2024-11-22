90656a929f0f231fe423345389eec7bf86483677
Alexandr Savca <alexandr.savca89@gmail.com>
2024-11-22 19:32:14 +0200

    holehe: fix missing dep and wrapper, relbump

--- holehe/holehe.sh
+++ holehe/holehe.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # holehe wrapper
 
-_CPATH=/opt/holehe
-_PYVER=$(python3 -V | grep -Po '\d\.\d')
-export PATH=$PATH:$_CPATH/bin
-export PYTHONPATH=$_CPATH:$_CPATH/usr/lib/python$_PYVER/site-packages
-
-python3 -c 'from holehe import core; core.main()' $@
+export PATH=$PATH:/opt/holehe/bin
+export PYTHONPATH=/opt/holehe/bin:/opt/holehe:/opt/holehe$(python3 -c 'import site; print(site.getsitepackages()[0])')
+python3 -c 'from holehe import core; core.main()' "$@"
 
 # End of file.
